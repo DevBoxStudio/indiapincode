2407315e8f18182ffef7223aa539f7c9bb71ccae
Saaheel Majethia <smajethia.com@gmail.com>
2023-04-20 13:24:10 +0530

    added static file for about us

--- src/Pincode.js
+++ src/Pincode.js
@@ -1,15 +1,11 @@
 import React from 'react';
+import About from './static/About'
 
 const Pincode = () => {
     return ( 
         <>
-            <h1>Search Pincode</h1>
-            <p>
-                A Postal Index Number <br />
-                (PIN; sometimes redundantly a PIN code) <br />
-                refers to a six-digit code in the Indian postal code system used by India Post. <br />
-                On 15 August 2022, the PIN system celebrated its 50th anniversary.
-            </p>
+            {/* <h1>Search Pincode</h1> */}
+            <About />
         </>
      );
 }
---
+++ src/static/About.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const About = () => {
+    return ( 
+        <>
+            <p className='content-desc'>
+                A Postal Index Number <br />
+                (PIN; sometimes redundantly a PIN code) <br />
+                refers to a six-digit code in the Indian postal code system used by India Post. <br />
+                On 15 August 2022, the PIN system celebrated its 50th anniversary.
+            </p>
+        </>
+     );
+}
+ 
+export default About;
