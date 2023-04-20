547234658277b81c03445cf54d19e85c2f845f20
Saaheel Majethia <smajethia.com@gmail.com>
2023-04-20 11:32:26 +0530

    added info about pincode

--- src/Pincode.js
+++ src/Pincode.js
@@ -4,7 +4,12 @@ const Pincode = () => {
     return ( 
         <>
             <h1>Search Pincode</h1>
-            <h3>Coming Soon</h3>
+            <p>
+                A Postal Index Number <br />
+                (PIN; sometimes redundantly a PIN code)[note 1] <br />
+                refers to a six-digit code in the Indian postal code system used by India Post. <br />
+                On 15 August 2022, the PIN system celebrated its 50th anniversary.
+            </p>
         </>
      );
 }
