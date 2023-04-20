bb60472207fe56c7554d3820b6e18214ab99797a
Saaheel Majethia <smajethia.com@gmail.com>
2023-04-20 10:49:55 +0530

    Added Initial files

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+import Pincode from "./Pincode";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Pincode />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Pincode.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Pincode = () => {
+    return ( 
+        <>
+            <h1>Search Pincode</h1>
+            <h3>Coming Soon</h3>
+        </>
+     );
+}
+ 
+export default Pincode;
